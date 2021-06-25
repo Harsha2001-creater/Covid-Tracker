47a02a5cafca3e667c82970b5a096379aaf04ff4
saisrivenkat <50038114+saisrivenkat@users.noreply.github.com>
2021-06-25 13:30:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [country, setcountry] = useState("india")
+  const [allcountry, setallcountry] = useState([])
+  const [countryinfo, setcountryinfo] = useState({})
+  useEffect(() => {
+    fetchcounntries()
+  }, [])
+
+  const fetchcounntries = async () => {
+    const url = `https://disease.sh/v3/covid-19/countries`
+    const counntry_data = await fetch(url)
+    const res = await counntry_data.json()
+    console.log(res)
+    setallcountry(res)
+
+  }
+  const submit = (e, country) => {
+    e.preventDefault();
+    const CountryName = country
+    const nameCapitalized = CountryName.charAt(0).toUpperCase() + CountryName.slice(1)
+    const id = allcountry.findIndex(li => li.country === nameCapitalized)
+    console.log(id)
+    const countrydata = allcountry[id]
+    console.log(countrydata)
+    setcountryinfo(countrydata)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input list="countries" name="myBrowser" onChange={(e) => setcountry(e.target.value)} />
+      <datalist id="countries">
+        {allcountry.map(li => (<option value={li.country} onChange={(e) => setcountry(e.target.value)} />))}
+      </datalist>
+      <button onClick={(e) => submit(e, country)}>Submit</button><br />
+      {countryinfo.active}
+
     </div>
   );
 }
